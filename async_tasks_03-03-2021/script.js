0be0861069b1b296d7eda3a1adc9b983f3c4a20e
kubasovainna <kubasovainna@mail.ru>
2021-03-09 18:22:45 +0300

    Update script.js

--- async_tasks_03-03-2021/script.js
+++ async_tasks_03-03-2021/script.js
@@ -1,28 +1,29 @@
 let result = '';
 const values = [10, 20, 30]
 const URLs = ['https://kodaktor.ru/api/m/',
-             'https://kodaktor.ru/api/MS2/',
-             'https://kodaktor.ru/api/MS3/'
-             ]
+ 'https://kodaktor.ru/api/MS2/',
+ 'https://kodaktor.ru/api/MS3/'
+]
 document
-  .querySelector('button')
-  .addEventListener('click',
-      async ({ target: t }) => { 
-        const headers = { "Content-Type": "application/json" };
-        t.textContent = 'Ожидается...';
-        let result=[];
-        let path='';
-          // здесь напишите код, последовательно отправляющий запросы
-          // согласно спецификации kodaktor.ru/async_tasks
-        for(i=0; i<URLs.length; i++){
+    .querySelector('button')
+    .addEventListener('click',
+        async ({ target: t }) => {
+         const headers = { "Content-Type": "application/json" };
+         t.textContent = 'Ожидается...';
+         let result=[];
+         let path='';
+         let resultFromServer;
+         // здесь напишите код, последовательно отправляющий запросы
+         // согласно спецификации kodaktor.ru/async_tasks
+         for(i=0; i<URLs.length; i++){
           if (result.length > 0) {
-            path = `/${result[result.length-1]}`;
+           path = `/${result[result.length-1]}`;
           } else{
-            path='';
+           path='';
           }
-          result =  await fetch(URLs[i]+values[i] + path, {headers}).then(x =>x.json());
-          result.push(result.result)
+          resultFromServer =  await fetch(URLs[i]+values[i] + path, {headers}).then(x =>x.json());
+          result.push(resultFromServer.result)
+         }
+         t.textContent = `Результат: ${result}`;
         }
-        t.textContent = `Результат: ${result}`;
-      }
-  ); 
+    );
